c6e7ae51429e8930c001c327c944b1e56f56085c
Mkups1 <m.kupilik1@share.epsb.ca>
2022-04-05 13:24:21 -0600

    Borders made

--- main.js
+++ main.js
@@ -7,4 +7,23 @@ cnv.height = 775;
 
 let muffin = document.getElementById("muffin")
 
-ctx.drawImage(muffin, 100, 137, 300, 300)
+ctx.drawImage(muffin, 50, 300, 300, 300)
+
+ctx.fillStyle = "rgb(125, 50, 0)"
+ctx.fillRect(0,25, 1250, 50)
+
+ctx.fillStyle = "rgb(125, 50, 0)"
+ctx.fillRect(400, 25, 50, 800)
+
+ctx.fillStyle = "rgb(125, 50, 0)"
+ctx.fillRect(800, 25, 50, 775)
+
+ctx.font = "30px arial"
+ctx.fillStyle = "black"
+ctx.fillText = ("Muffins:", 50, 50)
+
+muffin.addEventListener("mouseDown", moreMuffin)
+
+function moreMuffin() {
+    console.log("EEE")
+}
