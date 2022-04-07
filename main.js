d99f9abec749dae76ba725b87c9446256960e816
Mkups1 <m.kupilik1@share.epsb.ca>
2022-04-07 13:25:08 -0600

    Epic Background

--- main.js
+++ main.js
@@ -3,32 +3,26 @@
 let cnv = document.getElementById("canvas")
 let ctx = cnv.getContext("2d")
 cnv.width = 1250;
-cnv.height = 775;
+cnv.height = 850;
 
+let background = document.getElementById("background")
 let muffin = document.getElementById("muffin")
 let muffinAmount = 0
+let cursorUnlock = "rgb(225, 225, 225)"
+let bakerUnlock = "rgb(225, 225, 225)"
+let farmUnlock = "rgb(225, 225, 225)"
 
-ctx.drawImage(muffin, 50, 300, 300, 300)
-
-ctx.fillStyle = "rgb(125, 50, 0)"
-ctx.fillRect(0,25, 1250, 50)
-
-ctx.fillStyle = "rgb(125, 50, 0)"
-ctx.fillRect(400, 25, 50, 800)
-
-ctx.fillStyle = "rgb(125, 50, 0)"
-ctx.fillRect(800, 25, 50, 775)
-
-ctx.font = "30px Arial"
-ctx.fillStyle = "rgb(125, 50, 0)"
 
 requestAnimationFrame(loop)
 
 function loop(){
- ctx.fillStyle = "white"
- ctx.fillRect(0, 75, 400, 250)
+ ctx.drawImage(background, 0, 0, 1250, 850)
+ ctx.drawImage(muffin, 50, 300, 300, 300)
+ ctx.font = "30px Arial"
  ctx.fillStyle = "rgb(125, 50, 0)"
- ctx.fillText("Muffins:" + muffinAmount, 0, 150)
+ ctx.fillText("Muffins:" + muffinAmount, 25, 150)
+ ctx.fillStyle = "rgb(125,125,125)"
+ ctx.fillRect(914, 319, 313, 50) 
  requestAnimationFrame(loop)
 }
 
