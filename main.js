f8ffe76937d51b9c0f4d9e1970a8f17a00261ba2
Mkups1 <m.kupilik1@share.epsb.ca>
2022-05-04 13:26:11 -0600

    Tried to make cursor rotate

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ let bakerDiv = document.getElementById("baker-div")
 let muffinAmount = 0
 let cursorAmount = 0
 let bakerAmount = 0
+let displayCursorNum = document.getElementById("cursors")
+
 
 let cursorAngles = []
 let insideDotX = 90
@@ -37,31 +39,35 @@ function loop(){
  ctx.font = "30px Arial"
  ctx.fillStyle = "rgb(125, 50, 0)"
  ctx.fillText("Muffins:" + muffinAmount, 25, 50)
+ ctx.save()
+ ctx.translate(cnv.width/2,)
  // Draw cursor 
- if (cursorAmount >= 1) {
-   ctx.drawImage(cursorFloat, cursorX, cursorY, 25, 25)
-  } if (muffinAmount <= 10) {
+  if (muffinAmount <= 10) {
     cursorDiv.style.background="grey"
   }
  // Draw cursor spinning
  for(let n=0; n < cursorAngles.length; n++) {
+   ctx.rotate(cursorAngles[n] * Math.PI/180)
    cursorAngles[n] ++
    cursorX[n] = insideDotX + (distance * Math.cos((cursorAngles[n] - 90) * Math.PI / 180))
    cursorY[n] = insideDotY + (distance * Math.sin((cursorAngles[n] - 90) * Math.PI / 180))
    ctx.drawImage(cursorFloat, cursorX[n], cursorY[n], 25, 25)
  }
+ ctx.restore()
  requestAnimationFrame(loop)
 }
 
+
+// Make the muffins go up every second
 window.setInterval(
   function muffinSec(){
     muffinAmount++
     muffinAmount += cursorAmount
-    if (muffinAmount >= 10 * cursorAmount + 10){
+    if (muffinAmount >= 10 * cursorAmount + 5){
       cursorDiv.style.background="white"
     } if (muffinAmount >= 100){
       bakerDiv.style.background="white"
-    }
+    } 
     console.log(muffinAmount)
   }, 1000);
 
@@ -69,12 +75,12 @@ window.setInterval(
 document.addEventListener("mousedown", moreMuffin)
 
 
-
+// When you click make the muffins go up
 
 function moreMuffin(event) {
     if (event.x - cnv.getBoundingClientRect().x >= 20 && event.x - cnv.getBoundingClientRect().x <= 180 && event.y - cnv.getBoundingClientRect().y >= 110 && event.y - cnv.getBoundingClientRect().y <= 260) {
         muffinAmount++
-        if (muffinAmount >= 10 * cursorAmount + 10){
+        if (muffinAmount >= 10 * cursorAmount + 5){
           cursorDiv.style.background="white"
         } if (muffinAmount >= 100){
           bakerDiv.style.background="white"
@@ -83,13 +89,13 @@ function moreMuffin(event) {
     }
 }
 
-
+// Make buildings go up on click
 document.getElementById("cursor-div").addEventListener("mousedown", moreCursor)
 document.getElementById("baker-div").addEventListener("mousedown", moreBaker)
-
+// cursor
 function moreCursor() {
-  if (muffinAmount >= 10 * cursorAmount + 10) {
-   muffinAmount -= 10 * cursorAmount + 10
+  if (muffinAmount >= 10 * cursorAmount + 5) {
+   muffinAmount -= 10 * cursorAmount + 5
    if (cursorAmount > 0) {
     cursorAngles.push(cursorAngles[cursorAngles.length - 1] - 190)
     cursorX.push(cursorX[cursorX.length -1] - 15)
@@ -100,21 +106,24 @@ function moreCursor() {
      cursorY.push(15)
    }
    cursorAmount++
-
+   displayCursorNum.innerHTML = cursorAmount
    console.log(cursorAmount)
    
-  } if (muffinAmount <= 10 * cursorAmount + 10) {
+  } if (muffinAmount <= 10 * cursorAmount + 5) {
     cursorDiv.style.background="grey"
+  } if (muffinAmount <= 100 * bakerAmount + 100) {
+    bakerDiv.style.background="grey"
   }
  }
-
+ 
+//  baker
  function moreBaker() {
   if (muffinAmount >=100) {
    muffinAmount -= 100
    bakerAmount++
    console.log(bakerAmount)
    
-  } if (muffinAmount <= 100) {
+  } if (muffinAmount <= 100 * bakerAmount + 100) {
     bakerDiv.style.background="grey"
   }
  }
